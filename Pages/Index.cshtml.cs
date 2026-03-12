3c1afea909c8f8ca5b91a295cb903af6e3a62e6e
andorgast <andorgast@gmail.com>
2026-03-12 09:58:04 +0100

    creation of the project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dashboard.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
