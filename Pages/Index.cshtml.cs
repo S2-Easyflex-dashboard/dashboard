d2706100d58ecc698690b1d0992f592506283e12
Damei Chkadua <dameichkaduanl@gmail.com>
2026-03-19 11:31:00 +0100

    databse connected on main page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,24 +1,35 @@
 using dashboard.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
 
 namespace dashboard.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private string connectionString = "Server= 192.168.133.6;Database=s2group;User Id=dashboard;Password=1234;";
         public List<CallsViewModel> Calls { get; private set; } = new();
         public float ExternalCustomer { get; private set; } = 0;
         public float InternalCustomers { get; private set; } = 0;
         public float ExternalPercent { get; private set; }
         public float InternalPercent { get; private set; }
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-            var temp = new CallsViewModel(1, DateOnly.FromDateTime(DateTime.Now), "10.20.30", "no clue", 55, 9022);
-            var temp2 = new CallsViewModel(1, DateOnly.FromDateTime(DateTime.Now), "13.20.30", "no clue", 25, 9022);
-            var temp3 = new CallsViewModel(1, DateOnly.FromDateTime(DateTime.Now), "20.10.30", "no clue", 50, 9022);
-            Calls.Add(temp);
-            Calls.Add(temp2);
-            Calls.Add(temp3);
+            await using var conn = new MySqlConnection(connectionString);
+            await conn.OpenAsync();
+            await using var cmd = new MySqlCommand(
+                @"SELECT * FROM calls", conn
+            );
+            await using var reader = await cmd.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                Calls.Add(new CallsViewModel(reader.GetInt32(6), DateOnly.Parse(reader.GetString(1)), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5)));
+            }
+            conn.Close();
+
+
             foreach (var call in Calls)
             {
                 if (call.Ip.Split(".")[0] == "10")
@@ -32,6 +43,8 @@ namespace dashboard.Pages
             }
             ExternalPercent = (ExternalCustomer / (ExternalCustomer + InternalCustomers)) * 100;
             InternalPercent = (InternalCustomers / (InternalCustomers + ExternalCustomer)) * 100;
+
+            return Page();
         }
     }
 }
