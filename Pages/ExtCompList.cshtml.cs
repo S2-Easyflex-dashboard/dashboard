c354f6fdab1330c5160a6ad135c45c79861af51a
andorgast <andorgast@gmail.com>
2026-04-10 11:43:50 +0200

    changing id's to names and ip adresses to names if known

--- Pages/ExtCompList.cshtml.cs
+++ Pages/ExtCompList.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
 using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace dashboard.Pages;
 public class ExtCompList : PageModel
@@ -11,21 +12,22 @@ public class ExtCompList : PageModel
     public class IpInfo
     {
         // public List<IpAdress> Ip {get; private set;} = new();
-        public string Ip { get; private set; }
+        public string Ip { get; set; }
         public int Amount { get; private set; }
-        public List<int> Users { get; private set; }
-        public IpInfo(string ip, int amount, int user)
+        public List<int> CustomerIds { get; private set; }
+        public List<string> CustomerNames { get; private set; } = new();
+        public IpInfo(string ip, int amount, int customerIds)
         {
             Ip = ip;
             Amount = amount;
-            Users = [user];
+            CustomerIds = [customerIds];
         }
-        public void AddToCount(int number, int? user)
+        public void AddToCount(int number, int? customerIds)
         {
             Amount = Amount + number;
-            if (user != null)
+            if (customerIds != null)
             {
-                Users.Add((int)user);
+                CustomerIds.Add((int)customerIds);
             }
         }
     }
@@ -42,28 +44,28 @@ public class ExtCompList : PageModel
             @"SELECT * FROM calls", conn
         );
         using var reader = cmd.ExecuteReader();
-        if (!reader.HasRows)
+        if (reader.HasRows)
         {
-            conn.Close();
-            return;
-        }
-        while (reader.Read())
-        {
-            Calls.Add(new CallsViewModel(reader.GetInt32(6), DateOnly.Parse(reader.GetString(1)), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5)));
+            while (reader.Read())
+            {
+                Calls.Add(new CallsViewModel(reader.GetInt32(6), DateOnly.Parse(reader.GetString(1)), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5)));
+            }
         }
         conn.Close();
+
+        //sorting ip's on if they are used by multiple customers
         foreach(var call in Calls)
         {
             IpInfo? duplicateIp = null;
             bool duplicateAlreadyFound = false;
             foreach (var ipInformation in DuplicateIps)
             {
-                if (call.Ip == ipInformation.Ip && !ipInformation.Users.Contains(call.CustomerId))
+                if (call.Ip == ipInformation.Ip && !ipInformation.CustomerIds.Contains(call.CustomerId))
                 {
                     ipInformation.AddToCount(call.Amount, call.CustomerId);
                     duplicateAlreadyFound = true;
                 }
-                else if (call.Ip == ipInformation.Ip && ipInformation.Users.Contains(call.CustomerId))
+                else if (call.Ip == ipInformation.Ip && ipInformation.CustomerIds.Contains(call.CustomerId))
                 {
                     ipInformation.AddToCount(call.Amount, null);
                     duplicateAlreadyFound = true;
@@ -73,12 +75,12 @@ public class ExtCompList : PageModel
             {
                 foreach (var ipInformation in UniqueIps)
                 {
-                    if (call.Ip == ipInformation.Ip && !ipInformation.Users.Contains(call.CustomerId))
+                    if (call.Ip == ipInformation.Ip && !ipInformation.CustomerIds.Contains(call.CustomerId))
                     {
                         ipInformation.AddToCount(call.Amount, call.CustomerId);
                         duplicateIp = ipInformation;
                     }
-                    else if (call.Ip == ipInformation.Ip && ipInformation.Users.Contains(call.CustomerId))
+                    else if (call.Ip == ipInformation.Ip && ipInformation.CustomerIds.Contains(call.CustomerId))
                     {
                         ipInformation.AddToCount(call.Amount, null);
                     }
@@ -92,6 +94,41 @@ public class ExtCompList : PageModel
             else
             {
                 UniqueIps.Add(new IpInfo(call.Ip, call.Amount, call.CustomerId));
+            }
+        }
+
+        // replacing id and ip's with names
+        foreach (var ipInfo in DuplicateIps)
+        {
+            foreach (var id in ipInfo.CustomerIds)
+            {
+                using var connGetName = new MySqlConnection(connectionString);
+                connGetName.Open();
+                using var cmdGetName = new MySqlCommand(
+                    @"SELECT name FROM customers WHERE id = @id", connGetName
+                );
+                cmdGetName.Parameters.AddWithValue("@id", id);
+                using var readerGetName = cmdGetName.ExecuteReader();
+                if (readerGetName.Read())
+                {
+                    ipInfo.CustomerNames.Add(readerGetName.GetString(0));
+                }
+                connGetName.Close();
+            }
+            using var connGetIpName = new MySqlConnection(connectionString);
+            connGetIpName.Open();
+            using var cmdGetIpName = new MySqlCommand(
+                @"SELECT partner_name FROM partners WHERE ip_adress = @ip_adress", connGetIpName
+            );
+            cmdGetIpName.Parameters.AddWithValue("@ip_adress", ipInfo.Ip);
+            using var readerGetIpName = cmdGetIpName.ExecuteReader();
+            if (readerGetIpName.Read())
+            {
+                if (!readerGetIpName.IsDBNull(0))
+                {
+                    ipInfo.Ip = readerGetIpName.GetString(0);
+                }
+                connGetIpName.Close();
             }
         }
     }
