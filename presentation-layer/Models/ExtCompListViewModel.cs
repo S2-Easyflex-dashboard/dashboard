a85eb0f506a25354bf1126647e5a5914dce1efda
andorgast <andorgast@gmail.com>
2026-04-16 12:04:14 +0200

    presentation-layer should be functioning

---
+++ presentation-layer/Models/ExtCompListViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace presentation_layer.Models
+{
+    public class ExtCompListViewModel
+    {
+        public IpInfoViewModel[] DuplicateIps { get; private set; }
+
+        public ExtCompListViewModel(IpInfoViewModel[] duplicateIps)
+        {
+            DuplicateIps = duplicateIps;
+        }
+    }
+}
---
+++ presentation-layer/Models/IpInfoViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace presentation_layer.Models
+{
+    public class IpInfoViewModel
+    {
+        // public List<IpAdress> Ip {get; private set;} = new();
+        public string Ip { get; set; }
+        public int Amount { get; private set; }
+        public List<int> CustomerIds { get; private set; }
+        public List<string> CustomerNames { get; private set; } = new();
+
+        public IpInfoViewModel(string ip, int amount, int customerIds)
+        {
+            Ip = ip;
+            Amount = amount;
+            CustomerIds = [customerIds];
+        }
+    }
+}
